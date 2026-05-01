77dce277bafdeeea3ebc07d9d5cc39b1bc025682
Uzoezi <62175826+odueme@users.noreply.github.com>
2026-05-01 14:20:57 +0100

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,7 +4,7 @@ export const personalInfo = {
   email: "uodueme@gmail.com",
   github: "https://github.com/odueme",
   linkedin: "https://www.linkedin.com/in/uzoezi-odueme-27aa98222/",
-  profilePicture: "/public/ChatGPT Image May 1, 2026, 02_11_04 PM.png",
+  profilePicture: "/ChatGPT Image May 1, 2026, 02_11_04 PM.png",
   heroDescription:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 };
