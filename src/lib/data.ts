d2fa582f77e8b32158b4ef66663040c5fcd6945f
Uzoezi <62175826+odueme@users.noreply.github.com>
2026-05-15 23:13:27 +0100

    Update skills and remove awards from data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -62,12 +62,17 @@ export const education = [
 ];
 export const skills = {
   programmingLanguages: [
-    "LoremLang",
-    "IpsumScript",
-    "DolorLang",
-    "SitLang",
-    "AmetLang",
-    "ConsecteturLang",
+    "SIEM(Microsoft Sentinel, Splunk)"
+    "Wireshark",
+    "Sysmon",
+    "Process Monitor",
+    " MITRE ATT&CK ",
+    "Threat Detection",
+    "Phython"
+    "GDPR fundamentals"
+    "SOC Automation"
+    "Networking"
+    
   ],
   frontendDevelopment: [
     "LoremJS",
@@ -103,54 +108,4 @@ export const projects = [
 
 ];
 
-export const awards = [
-  {
-    name: "Lorem Ipsum Award",
-    issuer: "Lorem Organization",
-    date: "Jan 2020",
-    type: "International",
-    position: "First Place",
-  },
-  {
-    name: "Dolor Sit Amet Prize",
-    issuer: "Ipsum Foundation",
-    date: "Feb 2021",
-    type: "National",
-    position: "Runner-up",
-  },
-  {
-    name: "Consectetur Hackathon",
-    issuer: "Adipiscing Org",
-    date: "Mar 2022",
-    type: "National",
-    position: "Winner",
-  },
-  {
-    name: "Vestibulum Event",
-    issuer: "Vestibulum College",
-    date: "Apr 2022",
-    type: "National",
-    position: "First Prize",
-  },
-  {
-    name: "Curabitur Hackfest",
-    issuer: "Curabitur Institute",
-    date: "May 2022",
-    type: "National",
-    position: "Second Prize",
-  },
-  {
-    name: "Praesent Hacks",
-    issuer: "Praesent Group",
-    date: "Jun 2022",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Fusce Hack",
-    issuer: "Fusce Club",
-    date: "Jul 2022",
-    type: "National",
-    position: "Most Impactful Hack",
-  },
-];
+
