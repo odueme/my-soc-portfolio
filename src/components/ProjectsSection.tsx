26b73c8637a009e55569cf9621b1c6140f0b77ef
Uzoezi <62175826+odueme@users.noreply.github.com>
2026-06-11 15:55:58 +0100

    Update link text in ProjectsSection component

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -56,8 +56,7 @@ export default function ProjectsSection() {
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
                   >
-                    <Github className="h-4 w-4 mr-2 group-hover/link:rotate-12 transition-transform duration-300" />
-                    View on Projec 🔗
+                    View Project 🔗
                   </motion.a>
                 </CardFooter>
               </GlassCard>
